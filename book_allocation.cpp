81a037fe6b52c502b08e67e7c27d8d5b2c6827ef
Dhananjay Abbot <128765451+DJIITBH@users.noreply.github.com>
2025-08-05 18:44:42 +0530

    Create book_allocation.cpp
    book allocation problem with binary search. assuming search space is from 0 to sum of all elements.

---
+++ book_allocation.cpp
@@ -0,0 +1,52 @@
+bool isPossible(vector<int>& arr, int n, int m, int mid)
+{
+    int count=1;
+    int pagesum=0;
+
+    for(int i=0;i<n;i++)
+    {
+        if(pagesum+arr[i]<=mid)
+        {
+            pagesum+=arr[i];
+        }
+        else{
+            count++;
+            if(count>m || arr[i]>mid)
+            {
+                return false;
+            }
+            pagesum=arr[i];
+        }
+
+    }
+    return true;
+}
+
+int findPages(vector<int>& arr, int n, int m) {
+    int s = 0;
+    int sum=0;
+    int ans=-1;
+    for(int i=0;i<n;i++)
+    {
+        sum+=arr[i];
+    }
+    int e = sum;
+    int mid = s + (e-s)/2;
+    while(s<=e)
+    {
+        if(isPossible(arr, n, m,mid))
+        {
+            e=mid-1;
+            ans=mid;
+        }
+        else{
+            s=mid+1;
+        }
+        mid = s + (e-s)/2;
+    }
+    if(m>n)
+    {
+        ans=-1;
+    }
+    return ans;
+}
